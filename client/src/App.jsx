a50bfd3e71ad42cca6823ab9585d11360c5c4fcb
Suman Keross <suman.sur@keross.com>
2024-06-13 13:23:45 +0530

    protect route implementation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,32 @@
-import React, {lazy} from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import React, { lazy } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectRoute from "./components/auth/ProtectRoute";
 
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Chat = lazy(() => import("./pages/Chat"));
+const Groups = lazy(() => import("./pages/Groups"));
 
-const Home = lazy(()=>import('./pages/Home'))
-const Login = lazy(()=>import('./pages/Login'))
-const Chat = lazy(()=>import('./pages/Chat'))
-const Groups = lazy(()=>import('./pages/Groups'))
-
+let user = true;
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/login' element={<Login/>}></Route>
-        <Route path='/chat/:chatId' element={<Chat/>}></Route>
-        <Route path='/groups' element={<Groups/>}></Route>
+        <Route element={<ProtectRoute user={user} />}>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/chat/:chatId" element={<Chat />}></Route>
+          <Route path="/groups" element={<Groups />}></Route>
+        </Route>
+
+        <Route path="/login" element={  
+          <ProtectRoute user={!user} redirect="/">
+            <Login />
+          </ProtectRoute>
+        }></Route>
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,145 +1,208 @@
-import React, { useState } from 'react'
-import { Button, Container, Paper, TextField, Typography } from '@mui/material';
+import React, { useState } from "react";
+import {
+  Avatar,
+  Button,
+  Container,
+  IconButton,
+  Paper,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import CameraAltIcon from "@mui/icons-material/CameraAlt";
+import { VisuallyHiddenInput } from "../components/styles/StyledComponents";
 
 const Login = () => {
+  const [isLogin, setIsLogin] = useState(true);
 
-    const [isLogin, setIsLogin] = useState(true)
+  const toggleLogin = () => {
+    setIsLogin((prev) => (prev = !prev));
+  };
 
-    const toggleLogin = ()=>{
-        setIsLogin(prev => prev = !prev)
-    }
+  const [name, setName] = useState("");
+  const [bio, setBio] = useState("");
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
 
-    return (
-        <>
-            <Container component={"main"} maxWidth='xs' sx={{
-                height: '100vh',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center'
-            }}>
-                <Paper
-                    elevation={3}
+  const handleLogin = (e)=>{
+    e.preventDefault();
+  }
+
+  const handleSignUp = (e)=>{
+    e.preventDefault();
+  }
+
+  return (
+    <>
+      <Container
+        component={"main"}
+        maxWidth="xs"
+        sx={{
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Paper
+          elevation={3}
+          sx={{
+            padding: 4,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          {isLogin ? (
+            <>
+              <Typography variant="h5">Login</Typography>
+              <form
+                style={{
+                  width: "100%",
+                  marginTop: "1rem",
+                }}
+                onSubmit={handleLogin}
+              >
+                <TextField
+                  required
+                  fullWidth
+                  label="UserName"
+                  margin="normal"
+                  variant="outlined"
+                  value={userName}
+                  onChange={(e) => setUserName(e.target.value)}
+                ></TextField>
+
+                <TextField
+                  required
+                  fullWidth
+                  label="Password"
+                  type="password"
+                  margin="normal"
+                  variant="outlined"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                ></TextField>
+
+                <Button
+                  sx={{
+                    marginTop: "1rem",
+                  }}
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                >
+                  Login
+                </Button>
+                <Typography textAlign={"center"} m={"1rem"}>
+                  OR
+                </Typography>
+                <Button fullWidth variant="text" onClick={toggleLogin}>
+                  Sign Up Instead
+                </Button>
+              </form>
+            </>
+          ) : (
+            <>
+              <Typography variant="h5">Sign Up</Typography>
+              <form
+                style={{
+                  width: "100%",
+                  marginTop: "1rem",
+                }}
+                onSubmit={handleSignUp}
+              >
+                <Stack position={"relative"} margin={"auto"} width={"10rem"}>
+                  <Avatar
                     sx={{
-                        padding: 4,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center'
-                    }}>
-                    {isLogin ? (
-                        <>
-                            <Typography variant='h5'>Login</Typography>
-                            <form style={{
-                                width: '100%',
-                                marginTop: '1rem'
-                            }}>
+                      width: "10rem",
+                      height: "10rem",
+                      objectFit: "contain",
+                    }}
+                  ></Avatar>
+                  <IconButton
+                    sx={{
+                      position: "absolute",
+                      bottom: "0",
+                      right: "0",
+                      color: "rgb(255,255,255)",
+                      bgcolor: "rgb(0,0,0,0.5)",
+                      ":hover": {
+                        bgcolor: "rgb(0,0,0,0.7)",
+                      },
+                    }}
+                    component={"label"}
+                  >
+                    <CameraAltIcon></CameraAltIcon>
+                    <VisuallyHiddenInput type="file" />
+                  </IconButton>
+                </Stack>
 
-                                <TextField
-                                    required
-                                    fullWidth
-                                    label='UserName'
-                                    margin='normal'
-                                    variant='outlined'
-                                >
-                                </TextField>
+                <TextField
+                  required
+                  fullWidth
+                  label="Name"
+                  margin="normal"
+                  variant="outlined"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                ></TextField>
 
-                                <TextField
-                                    required
-                                    fullWidth
-                                    label='Password'
-                                    type='password'
-                                    margin='normal'
-                                    variant='outlined'
-                                >
-                                </TextField>
+                <TextField
+                  fullWidth
+                  label="Bio"
+                  margin="normal"
+                  variant="outlined"
+                  value={bio}
+                  onChange={(e) => setBio(e.target.value)}
+                ></TextField>
 
-                                <Button
-                                    sx={{
-                                        marginTop: '1rem'
-                                    }}
-                                    fullWidth
-                                    variant='contained'
-                                    color='primary'
-                                    type='submit'>
-                                    Login
-                                </Button>
-                                <Typography textAlign={'center'} m={'1rem'}>OR</Typography>
-                                <Button
-                                    fullWidth
-                                    variant='text'
-                                    onClick={toggleLogin}>
-                                    Sign Up Instead
-                                </Button>
-                            </form>
-                        </>
-                    ) : (
-                        <>
-                            <Typography variant='h5'>Sign Up</Typography>
-                            <form style={{
-                                width: '100%',
-                                marginTop: '1rem'
-                            }}>
+                <TextField
+                  required
+                  fullWidth
+                  label="UserName"
+                  margin="normal"
+                  variant="outlined"
+                  value={userName}
+                  onChange={(e) => setUserName(e.target.value)}
+                ></TextField>
 
-                                <TextField
-                                    required
-                                    fullWidth
-                                    label='Name'
-                                    margin='normal'
-                                    variant='outlined'
-                                >
-                                </TextField>
+                <TextField
+                  required
+                  fullWidth
+                  label="Password"
+                  type="password"
+                  margin="normal"
+                  variant="outlined"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                ></TextField>
 
-                                <TextField
-                                    fullWidth
-                                    label='Bio'
-                                    margin='normal'
-                                    variant='outlined'
-                                >
-                                </TextField>
+                <Button
+                  sx={{
+                    marginTop: "1rem",
+                  }}
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                >
+                  Sign Up
+                </Button>
+                <Typography textAlign={"center"} m={"1rem"}>
+                  OR
+                </Typography>
+                <Button fullWidth variant="text" onClick={toggleLogin}>
+                  Login Instead
+                </Button>
+              </form>
+            </>
+          )}
+        </Paper>
+      </Container>
+    </>
+  );
+};
 
-                                <TextField
-                                    required
-                                    fullWidth
-                                    label='UserName'
-                                    margin='normal'
-                                    variant='outlined'
-                                >
-                                </TextField>
-
-                                <TextField
-                                    required
-                                    fullWidth
-                                    label='Password'
-                                    type='password'
-                                    margin='normal'
-                                    variant='outlined'
-                                >
-                                </TextField>
-
-                                <Button
-                                    sx={{
-                                        marginTop: '1rem'
-                                    }}
-                                    fullWidth
-                                    variant='contained'
-                                    color='primary'
-                                    type='submit'>
-                                    Sign Up
-                                </Button>
-                                <Typography textAlign={'center'} m={'1rem'}>OR</Typography>
-                                <Button
-                                    fullWidth
-                                    variant='text'
-                                    onClick={toggleLogin}>
-                                    Login Instead
-                                </Button>
-                            </form>
-                        </>
-                    )}
-                </Paper>
-
-            </Container>
-        </>
-    )
-}
-
-export default Login
+export default Login;
