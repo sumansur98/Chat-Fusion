c795a86b95c179b3d71bc489a9ce89ab3b994574
Suman Sur <sumansur.cs98@gmail.com>
2024-06-18 01:15:57 +0530

    new group dialog

--- client/src/components/specifics/NewGroup.jsx
+++ client/src/components/specifics/NewGroup.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { Avatar, Button, Dialog, DialogTitle, IconButton, ListItem, Stack, TextField, Typography } from '@mui/material';
+import { sampleUsers } from '../../constants/SampleData';
+import UserItem from '../shared/UserItem';
+
 
 const NewGroup = () => {
+
+  const selectMemberHandler = () => {
+
+  }
+  const submitHandler = () => {
+
+  }
+
+  const [groupName, setGroupName] = useState("")
+
   return (
-    <div>NewGroup</div>
+    <Dialog open>
+      <Stack p={{ xs: '1rem', sm: '3rem' }} width={'25rem'} spacing={'2rem'}>
+        <DialogTitle textAlign={'center'} variant='h4'>New Group</DialogTitle>
+        <TextField label={'Group Name'} value={groupName} onChange={(e)=>setGroupName(e.target.value)}/>
+        <Typography variant='body1'>Members</Typography>
+        <Stack>
+          {
+            sampleUsers.map(user => (
+              <UserItem user={user} key={user._id}
+                handler={selectMemberHandler}
+              />
+            ))
+          }
+        </Stack>
+        <Stack direction={'row'} justifyContent={'space-evenly'}>
+          <Button variant='text' color='error' size='large'>Cancel</Button>
+          <Button variant='contained' size='large' onClick={submitHandler}>Create</Button>
+        </Stack>
+
+      </Stack>
+    </Dialog>
   )
 }
 
--- client/src/components/specifics/Notifications.jsx
+++ client/src/components/specifics/Notifications.jsx
@@ -1,15 +1,23 @@
-import { Dialog, DialogTitle, Stack, Typography } from '@mui/material'
-import React from 'react'
+/* eslint-disable react/display-name */
+import { Avatar, Button, Dialog, DialogTitle, IconButton, ListItem, Stack, Typography } from '@mui/material'
+import React, { memo } from 'react'
 import { sampleNotifications } from '../../constants/SampleData'
 
 const Notifications = () => {
+
+  const friendRequestHandler = ({ _id, accept }) => {
+
+  }
+
   return (
     <Dialog open>
-      <Stack p={{xs:'1rem', sm:'2rem'}} width={'25rem'}>
+      <Stack p={{ xs: '1rem', sm: '2rem' }} width={'25rem'}>
         <DialogTitle>Notifications</DialogTitle>
         {
           sampleNotifications.length > 0 ? (
-            <></>
+            sampleNotifications.map((i) => (
+              <NotificationItem sender={i.sender} _id={i._id} key={i._id} handler={friendRequestHandler} />
+            ))
           ) : (
             <Typography textAlign={'center'}>0 Notifications</Typography>
           )
@@ -19,5 +27,35 @@ const Notifications = () => {
     </Dialog>
   )
 }
+
+const NotificationItem = memo(({ sender, _id, handler }) => {
+  const { name, avatar } = sender;
+
+  return (
+    <ListItem>
+      <Stack direction={'row'} alignItems={'center'} spacing={'1rem'} width={"100%"}>
+        <Avatar />
+
+        <Typography
+          variant="body1"
+          sx={{
+            flexGlow: 1,
+            width: "100%"
+          }}
+        >
+          {`${name} sent you a friend request`}
+        </Typography>
+
+        <Stack direction={{
+          xs : 'column',
+          sm : 'row'
+        }}>
+          <Button onClick={()=>handler({_id, accept:true})}>Accept</Button>
+          <Button color='error' onClick={()=>handler({_id, accept:false})}>Reject</Button>
+        </Stack>
+      </Stack>
+    </ListItem>
+  )
+})
 
 export default Notifications
