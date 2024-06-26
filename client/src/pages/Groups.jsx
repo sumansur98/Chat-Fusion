e99eef51e931f1bfdef2cff5678807c62937e242
Suman Keross <suman.sur@keross.com>
2024-06-26 18:07:08 +0530

    groups page progress

--- client/src/pages/Groups.jsx
+++ client/src/pages/Groups.jsx
@@ -1,123 +1,244 @@
-import { Box, Drawer, Grid, IconButton, Stack, Tooltip, Typography } from '@mui/material'
-import { orange } from '@mui/material/colors'
-import React, { useState } from 'react'
-import { KeyboardBackspace as KeyboardBackspaceIcon, Menu as MenuIcon } from "@mui/icons-material";
-import { useNavigate } from "react-router-dom";
+import {
+  Box,
+  Button,
+  Drawer,
+  Grid,
+  IconButton,
+  Stack,
+  TextField,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import { orange } from "@mui/material/colors";
+import React, { useState, useEffect } from "react";
+import {
+  KeyboardBackspace as KeyboardBackspaceIcon,
+  Menu as MenuIcon,
+  Edit as EditIcon,
+  Done as DoneIcon,
+} from "@mui/icons-material";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { Link } from "../components/styles/StyledComponents";
 import AvatarCard from "../components/shared/AvatarCard";
-import { sampleChats } from '../constants/SampleData';
+import { sampleChats } from "../constants/SampleData";
 
 const Groups = () => {
+  const chatId = useSearchParams()[0].get("group");
+  console.log("chat id", chatId);
 
-	const chatId = 'afdasdfaf';
+  const navigate = useNavigate();
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
+  const [groupName, setGroupName] = useState("");
+  const [groupNameUpdatedValue, setGroupNameUpdatedValue] = useState("");
 
-	const navigate = useNavigate()
-	const navigateBack = () => {
-		navigate('/')
-	}
+  const navigateBack = () => {
+    navigate("/");
+  };
 
-	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+  const handleMobile = () => {
+    setIsMobileMenuOpen((prev) => !prev);
+  };
 
-	const handleMobile = () => {
-		setIsMobileMenuOpen(prev => !prev)
-	}
+  const handleMobileClose = () => setIsMobileMenuOpen(false);
 
-	const handleMobileClose = () => setIsMobileMenuOpen(false)
+  const updateGroupName = () => {
+    setIsEdit(false);
+  };
 
-	return (
-		<Grid container height={'100vh'}>
-			<Grid
-				item
-				sx={{
-					display: {
-						xs: 'none',
-						sm: 'block'
-					},
-					backgroundColor: 'orange'
-				}}
-				sm={4}
-			>
-				<GroupList myGroups={sampleChats} chatId={chatId} />
-			</Grid>
+  useEffect(() => {
+    setGroupName("Group Name");
+    setGroupNameUpdatedValue("Group Name");
 
-			<Grid
-				item
-				xs={12}
-				sm={8}
-				sx={{
-					display: 'flex',
-					flexDirection: 'column',
-					alignItems: 'center',
-					position: 'relative',
-					padding: '1rem 3rem'
-				}}>
+    return () => {
+      setGroupName("");
+      setGroupNameUpdatedValue("");
+      setIsEdit(false);
+    };
+  }, [chatId]);
 
-				<Box
-					sx={{
-						display: {
-							xs: 'block',
-							sm: 'none',
-							position: 'fixed',
-							right: '1rem',
-							top: '1rem'
-						}
-					}}>
-					<IconButton onClick={handleMobile}>
-						<MenuIcon />
-					</IconButton>
-				</Box>
+  const GroupName = (
+    <Stack
+      direction={"row"}
+      alignItems={"center"}
+      padding={"3rem"}
+      spacing={"1rem"}
+    >
+      {isEdit ? (
+        <>
+          <TextField
+            value={groupNameUpdatedValue}
+            onChange={(e) => setGroupNameUpdatedValue(e.target.value)}
+          />
+          <IconButton onClick={updateGroupName}>
+            <DoneIcon />
+          </IconButton>
+        </>
+      ) : (
+        <>
+          <Typography variant="h4">{groupName}</Typography>
+          <IconButton onClick={() => setIsEdit(true)}>
+            <EditIcon />
+          </IconButton>
+        </>
+      )}
+    </Stack>
+  );
 
-				<Tooltip title='back'>
-					<IconButton
-						sx={{
-							position: 'absolute',
-							top: '2rem',
-							left: '2rem',
-							bgcolor: 'rgba(0,0,0,0.8)',
-							color: 'white'
-						}}
-						onClick={navigateBack}>
-						<KeyboardBackspaceIcon />
-					</IconButton>
-				</Tooltip>
-			</Grid>
+  const ButtonGroup = (
+	<Stack
+	direction={{
+		xs:'column-reverse',
+		sm:'row'
+	}}
+	spacing={'1rem'}
+	p={{
+		xs:'0',
+		sm:'1rem',
+		md:'1rem 4rem'
+	}}>
+		<Button size='large' color="error">Delete Group</Button>
+		<Button size='large' variant="contained">Add Member</Button>
+	</Stack>
+  )
 
-			<Drawer sx={{
-				xs: 'block',
-				sm: 'none'
-			}} open={isMobileMenuOpen} onClose={handleMobileClose}>
-				<GroupList w='50vw' myGroups={sampleChats} chatId={chatId}/>
-			</Drawer>
+  return (
+    <Grid container height={"100vh"}>
+      <Grid
+        item
+        sx={{
+          display: {
+            xs: "none",
+            sm: "block",
+          },
+          backgroundColor: "orange",
+        }}
+        sm={4}
+      >
+        <GroupList myGroups={sampleChats} chatId={chatId} />
+      </Grid>
 
-		</Grid>
-	)
-}
+      <Grid
+        item
+        xs={12}
+        sm={8}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          position: "relative",
+          padding: "1rem 3rem",
+        }}
+      >
+        <Box
+          sx={{
+            display: {
+              xs: "block",
+              sm: "none",
+              position: "fixed",
+              right: "1rem",
+              top: "1rem",
+            },
+          }}
+        >
+          <IconButton onClick={handleMobile}>
+            <MenuIcon />
+          </IconButton>
+        </Box>
 
-const GroupList = ({ w = '100%', myGroups = [], chatId }) => {
-	return (
-		<Stack width={w}>
-			{
-				myGroups.length > 0 ? (
-					myGroups.map(group => <GroupListItem key={group._id} group={group} chatId={chatId} />)
-				) : (
-					<Typography textAlign={'center'} padding={'1rem'}>No Groups Found</Typography>
-				)
-			}
-		</Stack>
-	)
-}
+        <Tooltip title="back">
+          <IconButton
+            sx={{
+              position: "absolute",
+              top: "2rem",
+              left: "2rem",
+              bgcolor: "rgba(0,0,0,0.8)",
+              color: "white",
+            }}
+            onClick={navigateBack}
+          >
+            <KeyboardBackspaceIcon />
+          </IconButton>
+        </Tooltip>
 
-const GroupListItem = ({ group }) => {
-	const { name, avatar, _id, chatId } = group;
-	return (
-		<Link to={`?group=${_id}`}>
-			<Stack direction={'row'} spacing={'1rem'} alignItems={'center'}>
-				<AvatarCard avatar={avatar} />
-				<Typography>{name}</Typography>
+        {groupName && (
+          <>
+            {GroupName}
+            <Typography
+              margin={"2rem"}
+              alignSelf={"flex-start"}
+              variant="body1"
+            >
+              Members
+            </Typography>
 
-			</Stack>
-		</Link>
-	)
-}
+            <Stack
+              maxwidth={"45rem"}
+              width={"100%"}
+              boxSizing={"border-box"}
+              padding={{
+                sm: "1rem",
+                xs: "0",
+                md: "1rem 4rem",
+              }}
+              spacing={"2rem"}
+              bgcolor={"bisque"}
+              height={"50vh"}
+              overflow={"auto"}
+            >
+              {/* Members */}
+            </Stack>
 
-export default Groups
+            {ButtonGroup}
+          </>
+        )}
+      </Grid>
+
+      <Drawer
+        sx={{
+          xs: "block",
+          sm: "none",
+        }}
+        open={isMobileMenuOpen}
+        onClose={handleMobileClose}
+      >
+        <GroupList w="50vw" myGroups={sampleChats} chatId={chatId} />
+      </Drawer>
+    </Grid>
+  );
+};
+
+const GroupList = ({ w = "100%", myGroups = [], chatId }) => {
+  return (
+    <Stack width={w}>
+      {myGroups.length > 0 ? (
+        myGroups.map((group) => (
+          <GroupListItem key={group._id} group={group} chatId={chatId} />
+        ))
+      ) : (
+        <Typography textAlign={"center"} padding={"1rem"}>
+          No Groups Found
+        </Typography>
+      )}
+    </Stack>
+  );
+};
+
+const GroupListItem = ({ group, chatId }) => {
+  const { name, avatar, _id } = group;
+  return (
+    <Link
+      to={`?group=${_id}`}
+      onClick={(e) => {
+        if (chatId === _id) e.preventDefault();
+      }}
+    >
+      <Stack direction={"row"} spacing={"1rem"} alignItems={"center"}>
+        <AvatarCard avatar={avatar} />
+        <Typography>{name}</Typography>
+      </Stack>
+    </Link>
+  );
+};
+
+export default Groups;
